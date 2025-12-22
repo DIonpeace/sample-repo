559e18e1d58ec2740b3a71287eaa140d2d4fcda8
Pipat <pipat.pol@ku.th>
2025-12-22 14:42:19 +0700

    add the test

--- src/calculator.js
+++ src/calculator.js
@@ -16,5 +16,8 @@ function divide(a, b) {
     }
     return a / b;
 }
-
+console.log(add(30,10))
+console.log(multiply(5,2))
+console.log(subtract(5,10))
+console.log(divide(5,10))
 module.exports = { add, subtract, multiply, divide };
