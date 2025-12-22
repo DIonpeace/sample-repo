dcc3f96cbcd4509e32e74783ac45bbf233122545
Pipat <pipat.pol@ku.th>
2025-12-22 14:57:23 +0700

    add the test

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -5,10 +5,9 @@ describe('Calculator', () => {
         expect(add(2, 3)).toBe(5);
     });
 
-      test('adds 5 + 3 to equal 8', () => {
-        expect(add(2, 3)).toBe(5);
+    test('adds 5 + 3 to equal 8', () => {
+        expect(add(5, 3)).toBe(8);
     });
-
     
     test('subtracts 5 - 3 to equal 2', () => {
         expect(subtract(5, 3)).toBe(2);
